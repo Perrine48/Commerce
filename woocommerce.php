2800e58b7d0806a60339b8abebf30e10eca3775d
Perrine <perrine.salce@hotmail.com>
2018-11-14 10:06:12 +0100

    Display product category

--- woocommerce.php
+++ woocommerce.php
@@ -4,10 +4,18 @@ get_header(); ?>
 <main>
     <nav role="navigation">
         <?php
-            wp_nav_menu( array(
-                'menu_class'     => 'categorymenu',
-                'theme_location' => 'categorymenu',
-            ) );
+            // wp_nav_menu( array(
+            //     'menu_class'     => 'categorymenu',
+            //     'theme_location' => 'categorymenu',
+            // ) );
+
+            $categories = get_categories('taxonomy=product_cat&&exclude=16');
+              foreach($categories as $category){
+                  echo $category->name;
+              }
+
+
+
         ?>
     </nav>
     <?php woocommerce_content() ?>
